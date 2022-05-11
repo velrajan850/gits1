64117acd55551753c532a7a7b07c8f40beb7f466
velrajan850 <105338994+velrajan850@users.noreply.github.com>
2022-05-11 17:44:36 +0530

    Add files via upload

---
+++ op.java
@@ -0,0 +1,31 @@
+package gits;
+
+public class op{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("result of calling");
+		
+
+student s1 = new student();
+s1 .set (23555667);
+
+
+student s2 = new student();
+
+s2.setname("rajA");
+
+student s3 = new student();
+ s3.set( 1000000.4F);
+
+
+ 
+
+
+System.out.println ("result:"+s1.get() + s2.get() + s3.get());
+
+	}
+
+
+
+}
---
+++ student.java
@@ -0,0 +1,40 @@
+package gits;
+
+public class student {
+	private int accountnumber;
+	private String name;
+	private float amount;
+	
+	public void set ( int accountnumber) {
+		
+		this.accountnumber =accountnumber;
+			
+	}
+	
+	public int get()
+	{
+		return accountnumber;
+	}
+public void setname ( String name) {
+		
+		
+		this.name =name;
+	}
+	
+	public String get1 ()
+	{
+		return name;
+	}
+public void set ( float amount) {
+		
+		this.amount =amount;
+			
+	}
+	
+	public float get3 ()
+	{
+		return amount ;
+	}
+	
+
+}
